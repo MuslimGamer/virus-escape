5600a0f642db96e4640b482a72a8236339177274
PythonWantsBoop <ala2aldeen913@gmail.com>
2017-09-04 19:59:18 +0300

    Made the changes requested. Or at least, I tried to.

--- src/entities/player.js
+++ src/entities/player.js
@@ -7,74 +7,66 @@ Crafty.c('Player', {
             .color('white')
             .bind('KeyDown', this.moving);
 
-        
     },
     place: function() {
         // get random x, y coordinates to get a random tile
         // https://stackoverflow.com/a/4550514
-        this.wd = config("level").widthInTiles;
-        this.hg = config("level").heightInTiles;
+        this.tile_x = Math.floor(Math.random() * config('level').widthInTiles);
+        this.tile_y = Math.floor(Math.random() * config('level').heightInTiles);
 
-        this.rx = Math.floor(Math.random() * this.wd);
-        this.ry = Math.floor(Math.random() * this.hg);
-        
-
-        this.tileSize = config("tileSize");
-        this.pad = config("padding");
-
-        // goodness, this is dirty. Remind me to make a tile class.
-        // moves the player to a random tile
-        this.moveTo();
+        this.moveTo(this.tile_x, this.tile_y);
 
         return this;
+    },
 
+    moveTo: function(x, y) {
+        // set coordinate properties to given coordinates, just in case
+        this.tile_x = x;
+        this.tile_y = y;
+
+        this.move(x * (config("tileSize") + config("padding")) + config("padding") * 2, 
+                  y * (config("tileSize") + config("padding")) + config("padding") * 2);
     },
-    moveTo: function() {
-        this.move(this.rx * (this.tileSize + this.pad) + 16, this.ry * (this.tileSize + this.pad) + 16);
-    },
+
     moving: function(e) {
         var k = e.key;
 
         if (k == Crafty.keys.UP_ARROW || k == Crafty.keys.W) {
-            var it = this.ry;
-            var org = this.hg - 1; // minus one because the tiles are zero-based
-            var w = -1;
-
-            var ww = 'y';
+            var coordinate = this.tile_y;
+            var tileLength = config('level').heightInTiles - 1; // minus one because the tiles are zero-based
+            var movement = -1;
+            var which = 'y';
         }
         else if (k == Crafty.keys.DOWN_ARROW || k == Crafty.keys.S) {
-            var it = this.ry;
-            var org = this.hg - 1;
-            var w = 1;
-
-            var ww = 'y';
+            var coordinate = this.tile_y;
+            var tileLength = config('level').heightInTiles - 1;
+            var movement = 1;
+            var which = 'y';
         }
         else if (k == Crafty.keys.LEFT_ARROW || k == Crafty.keys.A) {
-            var it = this.rx;
-            var org = this.wd - 1;
-            var w = -1;
-
-            var ww = 'x';
+            var coordinate = this.tile_x;
+            var tileLength = config('level').widthInTiles - 1;
+            var movement = -1;
+            var which = 'x';
         }
         else if (k == Crafty.keys.RIGHT_ARROW || k == Crafty.keys.D) {
-            var it = this.rx;
-            var org = this.wd - 1;
-            var w = 1;
-
-            var ww = 'x';
+            var coordinate = this.tile_x;
+            var tileLength = config('level').widthInTiles - 1;
+            var movement = 1;
+            var which = 'x';
         } else {
             return;
         }
 
-        it += w;
-        if (!(it > org || it < 0)) {
-            if (ww == 'y') {
-                this.ry += w;
-            } else if (ww == 'x'){
-                this.rx += w;
+        var ghost = coordinate + movement;
+        // check if the movement would keep the player in the grid
+        if (!(ghost > tileLength || ghost < 0)) {
+            if (which == 'y') {
+                this.tile_y += movement;
+            } else {
+                this.tile_x += movement;
             }
-
-            this.moveTo();
+            this.moveTo(this.tile_x, this.tile_y);
         }
     }
 });
